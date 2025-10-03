b7aa6756937d84ce051074adcf6876b215b23a9c
yuchen <jane97622@gmail.com>
2025-10-03 02:37:04 +0200

    upload to handle concurrent read/write requests

--- main.go
+++ main.go
@@ -9,27 +9,63 @@ import (
 	"time"
 )
 
-var TimeStamp []string
+var writes = make(chan writeOp)
+var reads = make(chan readOp)
+
+type readOp struct {
+	resp chan string
+}
+type writeOp struct {
+	val  string
+	resp chan string
+}
 
 func fetchData(w http.ResponseWriter, req *http.Request) {
-	for i, s := range TimeStamp {
-		fmt.Println(i, ":", s)
+	read := readOp{
+		resp: make(chan string),
+	}
+	reads <- read
+	w.Header().Set("Content-Type", "text/plain")
+	_, err := fmt.Fprintf(w, <-read.resp)
+	if err != nil {
+		return
 	}
 }
 
 func saveData(w http.ResponseWriter, req *http.Request) {
 	data, err := io.ReadAll(req.Body)
-	TimeStamp = append(TimeStamp, string(data))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	write := writeOp{
+		val:  string(data),
+		resp: make(chan string),
+	}
+	writes <- write
+	w.Header().Set("Content-Type", "text/plain")
+	_, err = fmt.Fprintf(w, <-write.resp)
+	if err != nil {
+		return
 	}
 
 }
 
 func server() error {
+	go func() {
+		var TimeStamp string
+		for {
+			select {
+			case write := <-writes:
+				TimeStamp = write.val
+				write.resp <- "unix time stamp stored successfully\n"
+			case read := <-reads:
+				read.resp <- TimeStamp
+			}
+		}
+	}()
 	http.HandleFunc("/fetch", fetchData)
 	http.HandleFunc("/save", saveData)
-
 	if err := http.ListenAndServe(":8090", nil); err != nil {
 		return fmt.Errorf("ListenAndServe: %w", err)
 	}
@@ -46,12 +82,15 @@ func clientRequest(method string, client *http.Client) error {
 		request.Header.Set("Content-Type", "text/plain")
 		response, err := client.Do(request)
 		if err != nil {
-			return fmt.Errorf("failed to snede request: %w", err)
+			return fmt.Errorf("failed to send %v request: %w", http.MethodGet, err)
 		}
 		defer response.Body.Close()
 		if response.StatusCode != http.StatusOK {
 			return fmt.Errorf("server returned status code %d", response.StatusCode)
 		}
+		body, _ := io.ReadAll(response.Body)
+		fmt.Println("GET response:", string(body))
+
 	case http.MethodPost:
 		unixStr := strconv.FormatInt(time.Now().Unix(), 10)
 		request, err := http.NewRequest("POST", "http://127.0.0.1:8090/save", bytes.NewBufferString(unixStr))
@@ -61,13 +100,15 @@ func clientRequest(method string, client *http.Client) error {
 		request.Header.Set("Content-Type", "text/plain")
 		response, err := client.Do(request)
 		if err != nil {
-			return fmt.Errorf("failed to send request: %w", err)
+			return fmt.Errorf("failed to send %v request: %w", http.MethodPost, err)
 		}
 		defer response.Body.Close()
 		if response.StatusCode != http.StatusOK {
 			return fmt.Errorf("server returned status code %d", response.StatusCode)
 
 		}
+		body, _ := io.ReadAll(response.Body)
+		fmt.Println("POST response:", string(body))
 	}
 
 	return nil
@@ -83,7 +124,6 @@ func main() {
 	}()
 
 	time.Sleep(200 * time.Millisecond)
-
 	// client side simulations
 	client := &http.Client{}
 	err := clientRequest(http.MethodPost, client)
